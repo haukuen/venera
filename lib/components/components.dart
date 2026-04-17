9d8742e7847f3689198b105c45e8253d2e2cb14f
IRAS 17514 <me@exusiai.top>
2026-04-17 19:49:46 +0800

    feat: PC端侧边栏空白区域支持拖拽移动窗口 (#20)
    在侧边栏的顶部间距、中间Spacer和底部间距区域添加DragToMoveArea，
    使PC端(Windows/macOS/Linux)用户可以通过侧边栏空白区域拖拽移动窗口。

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -32,6 +32,7 @@ import 'package:venera/utils/ext.dart';
 import 'package:venera/utils/io.dart';
 import 'package:venera/utils/tags_translation.dart';
 import 'package:venera/utils/translations.dart';
+import 'package:window_manager/window_manager.dart';
 
 part 'image.dart';
 part 'appbar.dart';
